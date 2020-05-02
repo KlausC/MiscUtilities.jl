4c650bf2e3c878d218aa3d11c9e90034ca132adb
Klaus Crusius <klaus.crusius@web.de>
2020-05-02 22:57:01 +0200

    adapted to v1.0

--- src/inspections.jl
+++ src/inspections.jl
@@ -17,9 +17,10 @@ function reflect_data(obj::Array)
     elsize = Int32(reinterpret(UInt16, p[3:3])[2])
     offset = reinterpret(UInt32, p[3:3])[2]
     how = UInt8(flags & 0x0003)
-    ndims = UInt16((flags>>2) & 0x03ff)
-    pooled = Bool((flags>>12) & 0x0001)
-    ptrarray = Bool((flags>>13) & 0x0001)
+    ndims = UInt16((flags>>2) & 0x01ff)
+    pooled = Bool((flags>>11) & 0x0001)
+    ptrarray = Bool((flags>>12) & 0x0001)
+    hasptr = Bool((flags>>13) & 0x0001)
     isshared = Bool((flags>>14) & 0x0001)
     isaligned = Bool((flags>>15) & 0x0001)
     nrows = Int(p[4])
@@ -27,16 +28,24 @@ function reflect_data(obj::Array)
     @assert ndims == 1 && obj isa(Vector) || !(obj isa Vector)
     if obj isa Vector
         (data=data, length=length, how=how, ndims=ndims,
-        pooled=pooled, ptrarray=ptrarray, isshared=isshared, isaligned=isaligned,
+        pooled=pooled, ptrarray=ptrarray, hasptr=hasptr, isshared=isshared,
+        isaligned=isaligned,
         elsize=elsize, offset=offset, nrows=nrows, maxsize=maxsize)
     elseif ndims == 2
         (data=data, length=length, how=how, ndims=ndims,
-        pooled=pooled, ptrarray=ptrarray, isshared=isshared, isaligned=isaligned,
+        pooled=pooled, ptrarray=ptrarray, hasptr=hasptr, isshared=isshared,
+        isaligned=isaligned,
         elsize=elsize, offset=offset, nrows=nrows, ncols=maxsize)
+    elseif ndims == 0
+        (data=data, length=length, how=how, ndims=ndims,
+        pooled=pooled, ptrarray=ptrarray, hasptr=hasptr, isshared=isshared,
+        isaligned=isaligned,
+        elsize=elsize, offset=offset, nrows=1, ncols=1)
     else
         dims = unsafe_peek(obj, Int, ndims + 3)[4:end]
         (data=data, length=length, how=how, ndims=ndims,
-        pooled=pooled, ptrarray=ptrarray, isshared=isshared, isaligned=isaligned,
+        pooled=pooled, ptrarray=ptrarray, hasptr=hasptr, isshared=isshared,
+        isaligned=isaligned,
         elsize=elsize, offset=offset, nrows=dims[1], ncols=dims[2], dims=dims)
     end
 end
